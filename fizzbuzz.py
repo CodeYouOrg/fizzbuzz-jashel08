68e97a804e1f6b911af064c4f998fc9cb4bb83a7
jashel08 <jashel08@gmail.com>
2024-08-04 11:14:25 -0400

    Updated Branch Name

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,17 @@
 # add your code here
-
+fizzy = range(1,101)
+def fizzbuzz():
+    for x in fizzy:
+        if x % 3 == 0 and x % 5 == 0:
+            x = 'FizzBuzz'
+            print(x)
+        elif x % 5 == 0:
+            x = 'Buzz'
+            print(x)
+        elif x % 3 == 0:
+            x = 'Fizz'
+            print(x)
+        else:
+            print(x)
+    
+fizzbuzz()
